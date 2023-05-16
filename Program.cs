b5cd2f0cae45462e506a79afc3bfeacc7a2e8d68
Joseph Amir Illana Juárez <jillanajuarez@hotmail.com>
2023-05-15 21:17:45 -0600

    Primer repositorio

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Program {
+    static void Main(){
+        int suma = 0;
+        string entrada;
+
+        System.Console.Write("Ingrese el número positivo: ");
+        entrada = System.Console.ReadLine()!;
+
+        // Clausula de guarda, en caso de que el número no sea positivo
+        if(int.Parse(entrada) <= 0) {
+            return;
+        }
+
+        // Array que contiene los digitos del número
+        char[] digitos = entrada.ToCharArray();
+
+        for(int i=0; i<digitos.Length; i++) {
+            suma = suma + (int)Char.GetNumericValue(digitos[i]);
+
+            // Último dígito del número
+            if(i == digitos.Length-1) {
+                System.Console.Write(digitos[i]+" = ");
+                break;
+            }
+
+            System.Console.Write(digitos[i]+" + ");
+        }
+        System.Console.WriteLine(suma);
+    }
+}
